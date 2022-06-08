58b7924a0ba85d57483cc0aa7c05ee4e5b8753e3
Erkebulan Safiolla <erkebulan91@gmail.com>
2022-06-09 02:08:29 +0600

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,33 @@
+//показать/скрыть header-nav в мобильных устр
+$('.toggle-header-nav').click(function (){
+  $(this).toggleClass('active');
+  if($(this).hasClass('active')) {
+    $('.header-nav').addClass('show')
+  } else  {
+    $('.header-nav').removeClass('show')
+  }
+})
+
+$(document).ready(function () {
+  //настройка позиции и высоты на mobile
+  if ($(window).width() < 767) {
+    let height = $('header').height();
+    let menuHeight = $(window).height() - height;
+    $('.mobile-menu').css({
+      'top': height,
+      'height': menuHeight
+    })
+  }
+})
+
+//Открыть меню на mobile
+$('.toggle-mobile-menu').click(function () {
+  $(this).toggleClass('active');
+  if ($(this).hasClass('active')) {
+    $('.mobile-menu').addClass('show');
+    $('body').addClass('scroll-locked')
+  } else {
+    $('.mobile-menu').removeClass('show')
+    $('body').removeClass('scroll-locked')
+  }
+})
